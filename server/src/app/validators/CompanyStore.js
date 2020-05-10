5f0eb568984e72c88681fbb8bdef81b856db778c
Felipe D S Lima <felipe@monts.com.br>
2020-05-10 20:11:47 -0300

    adding CompanyStore

---
+++ server/src/app/validators/CompanyStore.js
@@ -0,0 +1,23 @@
+import * as Yup from 'yup'
+import Exception from '../exceptions/Exception'
+
+export default async (req, res, next) => {
+	try {
+		const schema = Yup.object().shape({
+			name: Yup.string().required(),
+			companyName: Yup.string().required(),
+			address: Yup.string().required(),
+			cnpj: Yup.string().max(14).required(),
+		})
+
+		await schema.validate(req.body, { abortEarly: false })
+
+		return next()
+	} catch (err) {
+		throw new Exception({
+			status: 400,
+			message: 'Validation fails',
+			data: err.inner,
+		})
+	}
+}
